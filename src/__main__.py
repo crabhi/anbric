24dbc36c4b8f7f2596a998479fe2c5bdde918fd8
Filip Sedlak <sedlak@flih.cz>
2021-12-21 17:28:54 +0100

    fy-xes

--- src/__main__.py
+++ src/__main__.py
@@ -1,25 +1,35 @@
 import os
 from pathlib import Path
+import sys
 
 import mitogen
 import mitogen.utils
+from ansible.playbook.task import Task
 
 keys_path = Path(__file__).parent.parent / 'test/docker'
+my_key = str(keys_path / 'test_key')
 
 
 def main():
-    mitogen.utils.log_to_file()
+    mitogen.utils.log_to_file(level='DEBUG' if '-v' in sys.argv else 'INFO')
 
     broker = mitogen.master.Broker()
     router = mitogen.master.Router(broker)
 
+    os.chmod(my_key, 0o600)
+
     try:
         z = router.ssh(hostname='127.0.1.1', username='user',
-                       identity_file=str(keys_path / 'test_key'),
-                       check_host_keys='accept',
-                       #ssh_args=f'-o UserKnownHostsFile={keys_path / "known_hosts"}',
+                       identity_file=my_key,
+                       #check_host_keys='accept',
+                       ssh_args=['-o', f'UserKnownHostsFile={keys_path / "known_hosts"}'],
                        python_path='python3')
         z.call(os.system, 'hostname')
+
+        from ansible.plugins.loader import action_loader
+        t = Task()
+        t.name = 'ping'
+        print(action_loader.get('ansible.legacy.normal', task=t))
     finally:
         broker.shutdown()
 
